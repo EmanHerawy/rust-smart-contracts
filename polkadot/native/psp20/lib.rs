67786d92d6ed8455f9125a27efd171b8b2a6bb9d
emanherawy <eman.herawy@gmail.com>
2023-02-20 01:55:55 +0200

    support erc20 functions

--- polkadot/native/psp20/lib.rs
+++ polkadot/native/psp20/lib.rs
@@ -1,74 +1,158 @@
+
 #![cfg_attr(not(feature = "std"), no_std)]
-
 use ink_lang as ink;
-
+ // a great resource to depend on https://use.ink/ink-vs-solidity/ 
 #[ink::contract]
 mod psp20 {
+    // you need to addd ink_prelude to crgo.toml to let it compile , by default it's not added  ink_prelude = { version = "3.3", default-features = false }
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+       use ink_prelude::string::String;
+       // to use mapping , you need to import it and after the new updates contract must drive `SpreadAllocate` to compile
+       use ink_storage::{
+        traits::{
+            SpreadAllocate,
+            // SpreadLayout,
+        },
+        Mapping,
+    };
+
     #[ink(storage)]
-    pub struct Psp20 {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    #[derive(SpreadAllocate)]
+    pub struct MyContract {
+        /// Assign a balance to every account ID
+        balances: Mapping<AccountId,Balance>,
+        allowances: Mapping<(AccountId,AccountId),Balance>,
+        total_supply:Balance,
+        max_supply:Balance,
+        admin:AccountId,
+        symbol:String,
+        name:String,
     }
-
-    impl Psp20 {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
+ #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
     }
+ #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        to: AccountId,
+        value: Balance,
+    }
+    impl MyContract {
+        /// Constructor to initialize the contract with an empty mapping.
+        #[ink(constructor, payable)]
+        pub fn new(max_supply_:Balance, name_:String,symbol_:String) -> Self {
+            //required for mappings
+            // this is to init with empty value
+            // ink_lang::utils::initialize_contract(|_| {})
+            // to init with valus 
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let psp20 = Psp20::default();
-            assert_eq!(psp20.get(), false);
+                contract.max_supply=max_supply_;
+                contract.symbol=symbol_;
+                contract.name=name_;
+                contract.admin=Self::env().caller();
+            })
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut psp20 = Psp20::new(false);
-            assert_eq!(psp20.get(), false);
-            psp20.flip();
-            assert_eq!(psp20.get(), true);
+
+        /// Retrieve the balance of the caller.
+        #[ink(message)]
+        pub fn balance_of(&self, owner:AccountId) -> Option<Balance> {
+            self.balances.get(owner)
         }
+        /// Retrieve the allowance.
+        #[ink(message)]
+        pub fn allowance(&self, from:AccountId, to:AccountId) -> Option<Balance> {
+            self.allowances.get((from,to))
+        }
+      /// Returns the token name.
+       #[ink(message)]
+       pub fn name(&self) -> String {
+           self.name.clone()
+       }
+      /// Returns the token symbole.
+       #[ink(message)]
+       pub fn symbol(&self) -> String {
+           self.symbol.clone()
+       }
+
+        /// Credit more money to the contract.
+        #[ink(message, payable)]
+        pub fn transfer(&mut self, to:AccountId,amount:Balance) {
+            let caller = self.env().caller();
+            self._transfer(caller,to,amount);
+        }
+
+        /// Withdraw all your balance from the contract.
+        pub fn withdraw(&mut self) {
+            let caller = self.env().caller();
+            assert_eq!(caller, self.admin,"UnAuthorized" );
+            let balance = Self::env().balance();
+            self.env().transfer(caller, balance).unwrap()
+        }
+
+        // private func
+     fn _mint(&mut self, to:AccountId, amount:Balance) {
+             let balance = self.balances.get(to).unwrap();
+             self.total_supply+=&amount;
+             self.balances.insert(to, &(balance + amount));
+             // emit event
+               Self::env().emit_event(Transfer{
+                from:self.env().account_id(), // temp use contract address unitl i could figure out how to set zero address
+                to:to,
+                value:amount
+            });
+        }   
+fn _transfer(&mut self, from:AccountId,to:AccountId,amount:Balance) {
+            // unwrap_or to handle null value
+            let balance = self.balances.get(from).unwrap_or(0);
+            let to_balance = self.balances.get(to).unwrap_or(0);
+          assert!(balance>amount,"Insufficient fund" );
+            self.balances.insert(from, &(balance -amount));
+            self.balances.insert(to, &(to_balance +amount));
+            // emit transfer event
+            Self::env().emit_event(Transfer{
+                from:from,
+                to:to,
+                value:amount
+            });
+        }
+
+        // public func
+
+  #[ink(message, payable)]
+        pub fn mint(&mut self) {
+            // check max supply
+            let caller = self.env().caller();
+            // TODO: check mint price
+            let endowment = self.env().transferred_value();
+            assert!(self.max_supply>= (self.total_supply+endowment),"");
+
+            self._mint(caller, endowment);
+        }
+
+
+          #[ink(message)]
+        pub fn transfer_from(&mut self, from:AccountId,to:AccountId,amount:Balance) {
+            let allowance = self.allowances.get((Self::env().caller(),from)).unwrap_or(0);
+            assert!(allowance>=amount,"Insufficient fund");
+            self._transfer(from,to,amount);
+        }
+          #[ink(message)]
+        pub fn approve(&mut self, spender:AccountId,value:Balance) {
+            let owner=Self::env().caller();
+           self.allowances.insert((owner,spender),&value);
+           Self::env().emit_event(Approval{
+            spender,owner,value
+           })
+        }
+
     }
 }
