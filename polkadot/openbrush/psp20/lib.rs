f555d4be6be3bf2233a4cf690877e40bddd4957d
emanherawy <eman.herawy@gmail.com>
2023-02-20 03:39:34 +0200

    psp20 token using openbrush

---
+++ polkadot/openbrush/psp20/lib.rs
@@ -0,0 +1,36 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+
+#[openbrush::contract]
+mod psp20 {
+ use ink_storage::traits::SpreadAllocate;
+    use openbrush::{
+        contracts::psp22::*,
+        traits::{
+            Storage,
+            // String,
+        },
+    };
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+     #[ink(storage)]
+    #[derive(Default, SpreadAllocate, Storage)]
+    pub struct Contract {
+        /// Stores a single `bool` value on the storage.
+       #[storage_field]
+        psp22: psp22::Data,
+    }
+impl PSP22 for Contract {}
+    impl Contract {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(total_supply: Balance) -> Self {
+            ink_lang::codegen::initialize_contract(|instance: &mut Contract| {
+                instance
+                    ._mint_to(instance.env().caller(), total_supply)
+                    .expect("Should mint");
+            })
+        }
+    }}
