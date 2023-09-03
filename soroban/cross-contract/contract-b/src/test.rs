bb4739559dc9afa8e476d568d40641dc62106649
Emanherawy <eman.herawy@gmail.com>
2023-09-03 16:58:43 +0300

    clean up

--- soroban/cross-contract/contract-b/src/test.rs
+++ soroban/cross-contract/contract-b/src/test.rs
@@ -28,12 +28,8 @@ assert_eq!(instance_b.get_a(&contract_a_id),3);
    let value= instance_b.a_by_invoke(&contract_a_id, &sig, &fn_args);
 // assert_eq!(instance_b.get_a(&contract_a_id),3);
 assert_eq!(u32::from_val(&env,&value),3);
-// instance.increment();
-// assert_eq!(instance.get_counter(),2);
-// assert_eq!(instance.decrement(),1);
-//  let logs = env.logger().all();
+ 
      std::println!("****************************************************************************");
-//      std::println!("{}", logs.join("\n"));
-
+ 
     
 }
